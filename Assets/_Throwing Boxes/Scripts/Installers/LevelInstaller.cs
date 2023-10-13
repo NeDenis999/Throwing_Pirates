2c0593157153116526b41cb7ecd24dea3f3b4c79
NeDenis999 <shatskikh.denis.2003@mail.ru>
2023-10-13 18:05:21 +0700

    Add Multiplayer

--- Assets/_Throwing Boxes/Scripts/Installers/LevelInstaller.cs
+++ Assets/_Throwing Boxes/Scripts/Installers/LevelInstaller.cs
@@ -7,36 +7,15 @@ namespace Throwing_Boxes
     public class LevelInstaller : MonoInstaller
     {
         [SerializeField]
-        private PlayerModel _playerModel;
-        
-        [SerializeField]
-        private List<QuestCondition> _questConditions;
-        
-        [SerializeField]
-        private HeroUpgradesManager _heroUpgradesManager;
-        
-        [SerializeField]
         private MoneyBank _moneyBank;
 
         [SerializeField]
         private UIManager _uiManager;
 
-        [SerializeField]
-        private SkillManager _skillManager;
-        
         public override void InstallBindings()
         {
-            BindFromInstance<PlayerModel>(_playerModel);
-            
-            foreach (var questCondition in _questConditions)
-            {
-                Container.Inject(questCondition);
-            }
-            
-            BindFromInstance<IHeroUpgradesManager>(_heroUpgradesManager);
             BindFromInstance<IMoneyBank>(_moneyBank);
             BindFromInstance(_uiManager);
-            BindFromInstance<ISkillManager>(_skillManager);
         }
 
         private void BindFromInstance<T>(T instance)
---
+++ Assets/_Throwing Boxes/Scripts/PlayerInstaller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Throwing_Boxes
+{
+    public class PlayerInstaller : MonoInstaller
+    {
+        [SerializeField]
+        private PlayerModel _playerModel;
+        
+        [SerializeField]
+        private List<QuestCondition> _questConditions;
+        
+        [SerializeField]
+        private HeroUpgradesManager _heroUpgradesManager;
+        
+        [SerializeField]
+        private SkillManager _skillManager;
+        
+        public override void InstallBindings()
+        {
+            BindFromInstance<PlayerModel>(_playerModel);
+            
+            foreach (var questCondition in _questConditions)
+            {
+                Container.Inject(questCondition);
+            }
+            
+            BindFromInstance<IHeroUpgradesManager>(_heroUpgradesManager);
+            BindFromInstance<ISkillManager>(_skillManager);
+        }
+        
+        private void BindFromInstance<T>(T instance)
+        {
+            Container
+                .Bind<T>()
+                .FromInstance(instance)
+                .AsSingle()
+                .NonLazy();
+        }
+    }
+}
